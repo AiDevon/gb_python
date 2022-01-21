8afef93e11bda473c7cb68585fbc4e4463c7ef08
g.turkin <turick7@gmail.com>
2022-01-22 01:46:25 +0300

    add task

--- task-5-6.py
+++ task-5-6.py
@@ -10,3 +10,31 @@
 # Физика: 30(л) — 10(лаб)
 # Физкультура: — 30(пр) —
 # Пример словаря: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
+
+with open("task-5-6.txt", encoding="utf-8") as file_data:
+    subject_dict ={}
+    for line in file_data:
+        subject = line[:line.find(":")]
+
+        l_index = line.find("(л)")
+        if l_index != -1:
+            lectures = line[l_index-3:l_index]
+        else:
+            lectures = 0
+
+        p_index = line.find("(пр)")
+        if p_index != -1:
+           practical = line[p_index-3:p_index]
+        else:
+            practical = 0
+
+        lec_index = line.find("(лаб)")
+        if lec_index != -1:
+            laboratory = line[lec_index-3:lec_index]
+        else:
+            laboratory = 0
+
+        sum_subject = int(lectures) + int(practical) + int(laboratory)
+        subject_dict[subject] = sum_subject
+
+    print(subject_dict)
