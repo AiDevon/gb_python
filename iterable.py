53a7d6823e52dd3393099bd029a35395c48cfc5b
g.turkin <turick7@gmail.com>
2022-01-31 17:49:54 +0300

    add less-7

---
+++ iterable.py
@@ -0,0 +1,17 @@
+class MyIterable:
+    def __init__(self, counter: int):
+        self.counter = counter
+        self.start = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.start += 1
+        if self.start >= self.counter:
+            raise StopIteration
+        else:
+            return self.start
+
+for x in MyIterable(10):
+    print(x)
